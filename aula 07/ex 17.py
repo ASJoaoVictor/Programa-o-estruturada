a1bc4391a98931b5def8e125f07abe5c2b09bec5
ASJoaoVictor <victor.sidiao@academico.ifpb.edu.br>
2025-06-01 18:58:48 +0000

    refatoração: ex 17

--- aula 07/ex 17.py
+++ aula 07/ex 17.py
@@ -1,17 +1,20 @@
 """Crie uma função chamada split_custom que aceita uma string e um caractere de separação como argumentos e retorna uma lista de substrings separadas pelo caractere de separação. Ela deve ter o mesmo comportamento que o método str.split()."""
 
-def split_custom(text: str, carac: str = " ") -> list:
-    newText = []
-    teste = ""
-    for i in text:
-        if i != carac or i == text[-1]:
-            teste += i
+def split_custom(texto: str, carac: str = " ") -> list:
+    newtexto = []
+    palavra = ""
+    for i, letra in enumerate(texto):
+        if letra == carac:
+            newtexto.append(palavra)
+            palavra = ""
+        elif i == len(texto) - 1:
+            palavra += letra
+            newtexto.append(palavra)
         else:
-            newText.append(teste)
-            teste = ""
-    return newText
+            palavra += letra
+    return newtexto
 
-frase = "isso é um teste da função split"
+frase = "isso é um palavra da função split"
 
-print(frase.split("s"))
-print(split_custom(frase, "s"))
+print(frase.split())
+print(split_custom(frase))

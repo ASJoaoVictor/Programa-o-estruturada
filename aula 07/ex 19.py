b389d237d205ac577c81f5de4bde3c10436137c2
ASJoaoVictor <victor.sidiao@academico.ifpb.edu.br>
2025-06-08 14:07:38 +0000

    refaroração: ex 19

--- aula 07/ex 19.py
+++ aula 07/ex 19.py
@@ -5,19 +5,20 @@ str.replace()."""
 
 def replace_custom(texto: str, antigo: str,novo: str):
     novo_texto = ""
-    for i in range(0, len(texto)):
-        if (texto[i:i + len(novo)].endswith(antigo) or texto[i:i + len(novo)].startswith(antigo)):
-            novo_texto += novo
-            i += len(novo)
-        else:
-            novo_texto += texto[i]
-
+    i = 0
+    while True:
         if len(texto) == i:
             break
+        elif (texto[i:i + len(antigo)] == antigo or texto[i:i + len(novo)] == antigo):
+            novo_texto += novo
+            i += len(antigo)
+        else:
+            novo_texto += texto[i]
+            i += 1
     return novo_texto
 
 
 texto = "O céu é azul. Azul é minha cor favorita."
 
-print(texto.replace("azul", "verde"))
-print(replace_custom(texto, "azul", "verde"))
+print(texto.replace("azul", "rosa"))
+print(replace_custom(texto, "azul", "rosa"))

c0673a389ae5445910141a4b18aebe093fcb82f9
ASJoaoVictor <victor.sidiao@academico.ifpb.edu.br>
2025-06-11 22:13:51 +0000

    aula 9: refatoração da quests̃o 2

--- aula_09/exercicios/ex_02.py
+++ aula_09/exercicios/ex_02.py
@@ -7,6 +7,7 @@ Faça uma iteração na lista para verificar quantos destes números são iguais
 
 from random import randint
 
+quantLancamentos = int(input("Digite a quantidade de lançametos: "))
 lancamentos = []
 
 def verificar(lista: list, num:int) -> int:
@@ -16,7 +17,15 @@ def verificar(lista: list, num:int) -> int:
             count += 1
     return count
 
-for i in range(0, 50):
+for i in range(0, quantLancamentos):
     lancamentos.append(randint(1, 6))
 
-print(verificar(lancamentos, 6))
+
+print(f"A frequência esperada é {(1/6)*quantLancamentos}")
+print(f"1 => {verificar(lancamentos, 1)}")
+print(f"2 => {verificar(lancamentos, 2)}")
+print(f"3 => {verificar(lancamentos, 3)}")
+print(f"4 => {verificar(lancamentos, 4)}")
+print(f"5 => {verificar(lancamentos, 5)}")
+print(f"6 => {verificar(lancamentos, 6)}")
+
